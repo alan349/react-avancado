b5dfe05b62be6649b1cf32cb35abfbf558d577e4
Alan Eugeniom <alan.349@hotmail.com>
2021-11-10 19:40:36 -0300

    updates routes to react-router-dom 6

--- src/App.js
+++ src/App.js
@@ -1,43 +1,51 @@
 import React from 'react';
-
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import LoginPage from './pages/LoginPage';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route exact path="/">
-        </Route>
+        <Route path="/" element={<LoginPage />} />
 
-        <PrivateRoute path="/motoristaList">
-        </PrivateRoute>
-        <PrivateRoute path="/motoristaAdd">
-        </PrivateRoute>
-        <PrivateRoute path="/motoristaUpdate/:id">
-        </PrivateRoute>
-        <PrivateRoute path="/motoristaVeiculos/:id">
-        </PrivateRoute>
-        <PrivateRoute path="/veiculoList">
-        </PrivateRoute>
-        <PrivateRoute path="/veiculoAdd">
-        </PrivateRoute>
-        <PrivateRoute path="/veiculoUpdate/:id">
-        </PrivateRoute>
+        <Route element={
+          <PrivateRoute>
+          </PrivateRoute>
+        } path="/motoristaList" />
+        <Route element={
+          <PrivateRoute>
+          </PrivateRoute>
+        } path="/motoristaAdd" />
+        <Route element={
+          <PrivateRoute>
+          </PrivateRoute>
+        } path="/motoristaUpdate/:id" />
+        <Route element={
+          <PrivateRoute>
+          </PrivateRoute>
+        } path="/motoristaVeiculos/:id" />
+        <Route element={
+          <PrivateRoute>
+          </PrivateRoute>
+        } path="/veiculoList" />
+        <Route element={
+          <PrivateRoute>
+          </PrivateRoute>
+        } path="/veiculoAdd" />
+        <Route element={
+          <PrivateRoute>
+          </PrivateRoute>
+        } path="/veiculoUpdate/:id" />
 
-        <Route>
-          404
-        </Route>
+        <Route path="/*" element={"404"} />
       </Routes>
     </BrowserRouter>
   );
 }
 
-function PrivateRoute({ children, ...rest }) {
-  return (
-    <Route {...rest}>
-      { children }
-    </Route>
-  )
+function PrivateRoute({ children }) {
+  return true;
 }
 
 export default App;
